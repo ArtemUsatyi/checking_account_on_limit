7c2dd67a22ac5a96bec09f672355f4ece222a246
artemowork <artem180791@mail.ru>
2024-03-22 22:40:49 +0300

    Микросервис по интеграции в существующую систему.

---
+++ src/test/java/com/example/checking_account_on_limit/CheckingAccountOnLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checking_account_on_limit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingAccountOnLimitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
