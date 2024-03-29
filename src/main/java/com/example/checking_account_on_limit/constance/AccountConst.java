6495037fae332ac7f2d62f62348e2266ec906b6d
artemowork <artem180791@mail.ru>
2024-03-29 19:56:20 +0300

    Добавление новой переменной

--- src/main/java/com/example/checking_account_on_limit/constance/AccountConst.java
+++ src/main/java/com/example/checking_account_on_limit/constance/AccountConst.java
@@ -7,6 +7,7 @@ public class AccountConst {
     private final static String BASE_URL = "/api";
     public final static String REGISTRATION_TRANSACTION = BASE_URL + "/registration_transaction";
     public final static String SETTING_BALANCE_LIMIT = BASE_URL + "/setting_balance_limit";
+    public final static String TEST_SETTING_CONVERSION = "/test";
 
     /**
      * Message constance
@@ -14,4 +15,6 @@ public class AccountConst {
     public final static String NON_VALID_FIELD_CLIENT = "поле клиента не валидны.";
     public final static String SET_CURRENT_LIMIT = "Текущий лимит установлен";
     public final static String USER_NOT_CHANGE_LIMIT = "Лимит уже изменен. Клиент не может изменить лимит.";
+    public final static String JSON_MESSAGE_ERROR = "Возникла ошибка при получении информации.";
+    public final static String STATUS_OK = "Сохранение пары валюты проиошло успешно";
 }
